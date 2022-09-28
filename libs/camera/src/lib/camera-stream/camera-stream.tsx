02b118d9b18732751ed6dfd39f2d9c3545f47f92
Alessia <alessia.corsini@meso.design>
2022-09-28 14:32:24 +0200

    refactor: minimal styling

--- libs/camera/src/lib/camera-stream/camera-stream.tsx
+++ libs/camera/src/lib/camera-stream/camera-stream.tsx
@@ -2,12 +2,12 @@ import { useRef, useEffect } from 'react';
 import { useUserMedia } from '../camera';
 import { CaptureOptions } from '../types';
 
+import styles from './camera-stream.module.scss';
 
 export interface CameraStreamProps {
   captureOptions?: CaptureOptions,
   debug?: boolean
 }
-
 
 export const CameraStream = (props: CameraStreamProps) => {
   const { debug } = props
@@ -20,16 +20,16 @@ export const CameraStream = (props: CameraStreamProps) => {
     videoElRef.current.srcObject = mediaStream;
   }, [mediaStream])
 
+
   return (
-    <div>
-      <video
+    <div className={styles['camera-stream']}>
+      <video className={styles['camera-stream__video']}
         ref={videoElRef}
         autoPlay
         playsInline
         muted
       />
-
-      {debug && <div>Permission status: {status}</div>}
+      {debug && <div className={styles['camera-stream__debug']}>Permission status: {status}</div>}
     </div>
   );
 };
