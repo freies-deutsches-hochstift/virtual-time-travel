edb5c2791b0ca2b0f8adab150eeba17c78c9f3b9
Alessia <alessia.corsini@meso.design>
2022-10-06 14:24:03 +0200

    deleted obsolete examples folder

--- apps/geo-ar/src/app/examples/examples.spec.tsx
+++
@@ -1,9 +0,0 @@
-import { render } from '@testing-library/react';
-import Examples from './examples';
-
-describe('Examples', () => {
-  it('should render successfully', () => {
-    const { baseElement } = render(<Examples />);
-    expect(baseElement).toBeTruthy();
-  });
-});
