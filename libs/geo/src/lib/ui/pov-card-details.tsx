d4c9b21326e58dc6afc87a943399717a11ef26f2
Alessia <alessia.corsini@meso.design>
2022-11-08 13:27:18 +0100

    feat: card detail animation

--- libs/geo/src/lib/ui/pov-card-details.tsx
+++ libs/geo/src/lib/ui/pov-card-details.tsx
@@ -1,38 +1,59 @@
 import { EnhancedPov } from "@virtual-time-travel/geo-types";
 import {
   Button,
+  FadeAnimation,
   Icon,
   Icons,
   Markdown,
-  StyledCardDetails,
   StyledCardDetailsContent,
   StyledCardDetailsInner,
 } from "@virtual-time-travel/ui";
+import { AnimatePresence } from "framer-motion";
+import tw from "twin.macro";
 
 export interface PovCardDetailsProps {
-  pov: EnhancedPov;
+  pov: EnhancedPov | null;
   onClose: (e: unknown) => void;
 }
 
 export function PovCardDetails({ pov, onClose }: PovCardDetailsProps) {
+  return (
+    <AnimatePresence>
+      {!!pov && (
+        <FadeAnimation
+          key={pov?.id}
+          css={tw`absolute inset-0 z-top flex items-center justify-center`}
+        >
+          <PovCardDetailsInner {...{ pov, onClose }} />
+        </FadeAnimation>
+      )}
+    </AnimatePresence>
+  );
+}
+
+interface PovCardDetailsInnerProps {
+  pov: EnhancedPov;
+  onClose: (e: unknown) => void;
+  show?: boolean;
+}
+
+function PovCardDetailsInner({ pov, onClose }: PovCardDetailsInnerProps) {
   const { contentUrl } = pov;
 
   return (
-    <StyledCardDetails>
-      <StyledCardDetailsInner>
-        <StyledCardDetailsContent>
-          <Markdown
-            asSlideshow
-            contentUrl={contentUrl}
-            fallbackComponent={<MissingPovFallback {...{ pov }} />}
-          />
-        </StyledCardDetailsContent>
+    <StyledCardDetailsInner>
+      <StyledCardDetailsContent>
+        <Markdown
+          asSlideshow
+          contentUrl={contentUrl}
+          fallbackComponent={<MissingPovFallback {...{ pov }} />}
+        />
+      </StyledCardDetailsContent>
 
-        <Button highlight {...{ onClick: onClose, rounded: false }}>
-          <Icon type={Icons.Close} />
-        </Button>
-      </StyledCardDetailsInner>
-    </StyledCardDetails>
+      <Button highlight {...{ onClick: onClose, rounded: false }}>
+        <Icon type={Icons.Close} />
+      </Button>
+    </StyledCardDetailsInner>
   );
 }
 
